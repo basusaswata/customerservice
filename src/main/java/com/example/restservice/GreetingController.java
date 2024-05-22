549522a1f24a584e5a4bc0fa49c8c5ac0e40f17d
Saswata Basu <basu.saswata@gmail.com>
2024-05-22 14:00:49 -0500

    initial commit

---
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,30 @@
+package com.example.restservice;
+
+import java.security.Principal;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+	
+    @RequestMapping(value = "/customer")
+    public String user(Model model, Principal principal) {
+       // UserDetails currentUser = (UserDetails) ((Authentication) principal).getPrincipal();
+        return "customer service";
+    }
+}
